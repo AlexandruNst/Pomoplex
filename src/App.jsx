62a3ee36922bd7656aa4f1e45169eaa30741e53e
alexandrunst <nastase.alexandru100@gmail.com>
2023-03-24 22:05:47 +0000

    Separate Config from Pomo timer

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import { Pomodoro, Tasks, TimerConfig, PomodoroButton } from './components'
 
 function App() {
 
+  const [config, setConfig] = useState({
+    pomoTime: 7,
+    shortBreak: 10,
+    longBreak: 20
+  })
+
   const [minutes, setMinutes] = useState(5)
   const [timerTicking, setTimerTicking] = useState(true)
 
   function toggleTimer() {
-    if (minutes > 0) setTimerTicking(oldTimerTicking => !oldTimerTicking)
+    if (minutes <= 0) setMinutes(config.pomoTime)
+    setTimerTicking(oldTimerTicking => !oldTimerTicking)
     // if (minutes === 0) setMinutes(5)
   }
 
@@ -27,11 +34,19 @@ function App() {
 
 
   function incrementTimer() {
-    setMinutes(oldMinutes => oldMinutes + 1)
+    // setMinutes(oldMinutes => oldMinutes + 1)
+    setConfig(oldConfig => ({
+      ...oldConfig,
+      pomoTime: oldConfig.pomoTime + 1
+    }))
   }
 
   function decrementTimer() {
-    setMinutes(oldMinutes => oldMinutes > 1 ? oldMinutes - 1 : 1)
+    // setMinutes(oldMinutes => oldMinutes > 1 ? oldMinutes - 1 : 1)
+    setConfig(oldConfig => ({
+      ...oldConfig,
+      pomoTime: oldConfig.pomoTime > 1 ? oldConfig.pomoTime - 1 : 1
+    }))
   }
 
   return (
@@ -46,8 +61,7 @@ function App() {
         timerTicking={timerTicking}
       />
       <TimerConfig
-        minutes={minutes}
-        setMinutes={setMinutes}
+        minutes={config.pomoTime}
         incrementTimer={incrementTimer}
         decrementTimer={decrementTimer}
       />
