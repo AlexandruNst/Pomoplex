66adb6da06ff2262c612442cd92ecb4f411e6bfd
Alexandru Nastase <nastase.alexandru100@gmail.com>
2023-07-18 19:32:06 +0100

    Added Task styles

---
+++ src/components/TasksUnit/Task/Task.jsx
@@ -0,0 +1,7 @@
+import "./Task.scss"
+
+export default function Task(props) {
+    return (
+        <section className="task">{props.task}</section>
+    )
+}
--- src/components/TasksUnit/TasksList/TasksList.jsx
+++ src/components/TasksUnit/TasksList/TasksList.jsx
@@ -1,10 +1,13 @@
+import Task from '../Task/Task'
 import './TasksList.scss'
 
 export default function TasksList(props) {
     return (
         <article>
             {props.tasks.map((task, index) => (
-                <section key={index}>{task}</section>
+                <Task
+                    task={task}
+                />
             ))}
         </article>
     )
