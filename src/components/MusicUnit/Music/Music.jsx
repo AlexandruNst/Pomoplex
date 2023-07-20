8dc422521a54ef5a061f31c392f19034e638d3a7
Alexandru Nastase <nastase.alexandru100@gmail.com>
2023-07-20 19:00:36 +0100

    Added Spotify Widget

--- src/components/MusicUnit/Music/Music.jsx
+++ src/components/MusicUnit/Music/Music.jsx
@@ -1,10 +1,12 @@
 import "./Music.scss"
+import { Spotify } from "react-spotify-embed"
 
 export default function Music() {
     return (
         <div className="music">
             <h2 className="title">Music</h2>
             <p>Work in Progress, coming soon!</p>
+            <Spotify link="https://open.spotify.com/album/1pgFg0Ikr5FxCxkbHpDGjC?si=bAPGYj26Rl2KfctAz9ooqQ" />
         </div>
     )
 }
