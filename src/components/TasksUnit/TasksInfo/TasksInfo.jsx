dca35102b39eea5256bfbf6409ffa39a2f59f191
Alexandru Nastase <nastase.alexandru100@gmail.com>
2023-07-27 20:10:17 +0100

    Added no tasks custom text

--- src/components/TasksUnit/TasksInfo/TasksInfo.jsx
+++ src/components/TasksUnit/TasksInfo/TasksInfo.jsx
@@ -4,8 +4,15 @@ export default function TasksInfo(props) {
     return (
         <div className="tasks-info">
             <aside>
-                <span className="tasks-info-opacity">{`Completed Tasks: ${props.completedTasks}/${props.totalTasks}`}</span>
-                <span id="party-emoji"></span>{props.totalTasks === props.completedTasks ? " 🎉" : ""}<span />
+                {props.totalTasks === 0 ?
+                    <span className="tasks-info-opacity">Add tasks by pressing the + button</span>
+                    :
+                    <>
+                        <span className="tasks-info-opacity">{`Completed Tasks: ${props.completedTasks}/${props.totalTasks}`}</span>
+                        <span id="party-emoji"></span>{props.totalTasks === props.completedTasks ? " 🎉" : ""}<span />
+                    </>
+                }
+
             </aside>
             {props.showTaskButtonInfo && <aside><span className="tasks-info-opacity">{props.taskButtonInfo}</span></aside>}
         </div>
