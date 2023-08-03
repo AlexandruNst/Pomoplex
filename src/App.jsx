bb81110e1b69a1a3573aa4e3819562bdb10c9425
Alexandru Nastase <nastase.alexandru100@gmail.com>
2023-08-03 16:35:20 +0100

    Fixes card borders

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,20 @@ function App() {
       <main className="app-wrapper">
         <article className="pomo-tasks">
           <div className="glass-card pomo-tasks-card">
+            <div className="card-border"></div>
+            <div className="card-bg"></div>
+            <div className="card-glass"></div>
+            <div className="card-gradient"></div>
             <Pomodoro />
             <Tasks />
           </div>
         </article>
         <aside className="music">
           <div className="glass-card music-card">
+            <div className="card-border"></div>
+            <div className="card-bg"></div>
+            <div className="card-glass"></div>
+            <div className="card-gradient"></div>
             <Music />
           </div>
         </aside>
